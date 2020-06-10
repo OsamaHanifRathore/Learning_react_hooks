0072994d886ce9dcbb35b90ba9f99295c0913b8d
OsamaHanifRathore <48179164+OsamaHanifRathore@users.noreply.github.com>
2020-06-10 20:55:37 +0500

    Start learning react

---
+++ src/Room.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+import './App.css';
+
+
+function Room() {
+const [isLite,setLite] = useState(true)
+  return (
+    <div> this is room </div>
+  );
+}
+
+export default Room;
